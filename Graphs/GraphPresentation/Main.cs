dbe0c0f9fc8d127aa978d0a9f02bbb796ae0ac75
camilofg <rc.forero@uniandes.edu.co>
2018-03-19 22:45:30 -0500

    Configurations

--- Graphs/GraphPresentation/Main.cs
+++ Graphs/GraphPresentation/Main.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.Xml;
 using GraphLibrary;
 
 namespace GraphPresentation
 {
 	public partial class Main : Form
 	{
+	    private string _routeFile;
 		public Main()
 		{
 			InitializeComponent();
@@ -15,41 +17,51 @@ namespace GraphPresentation
 		{
 			var option = new Options();
 
-			//XmlDocument doc = new XmlDocument();
-			//doc.Load("C:\\Users\\raul.forero\\Documents\\configs\\default.xml");
-			//var test = doc.SelectNodes("//feature[@name='DFS' and @manual='selected']").Count;
+		    if (ChkXml.Checked)
+		    {
+		        XmlDocument doc = new XmlDocument();
+		        doc.Load("C:\\Users\\raul.forero\\Documents\\configs\\default.xml");
+		        var test = doc.SelectNodes("//feature[@name='DFS' and @manual='selected']").Count;
 
-			//      option.SearchOption = doc.SelectNodes("//feature[@name='DFS' and @manual='selected']").Count != 0 ? SearchEnum.Dfs : (doc.SelectNodes("//feature[@name='BFS' and @manual='selected']").Count != 0 ? SearchEnum.Bfs : SearchEnum.NoSearch);
+		        option.SearchOption = doc.SelectNodes("//feature[@name='DFS' and @manual='selected']").Count != 0
+		            ? SearchEnum.Dfs
+		            : (doc.SelectNodes("//feature[@name='BFS' and @manual='selected']").Count != 0
+		                ? SearchEnum.Bfs
+		                : SearchEnum.NoSearch);
 
-			//option.DirectionOption = doc.SelectNodes("//feature[@name='Directed' and @manual='selected']").Count != 0
-			//    ? DirectionEnum.Directed
-			//    : DirectionEnum.Undirected;
+		        option.DirectionOption = doc.SelectNodes("//feature[@name='Directed' and @manual='selected']").Count != 0
+		            ? DirectionEnum.Directed
+		            : DirectionEnum.Undirected;
 
-			//option.WheightOption = doc.SelectNodes("//feature[@name='Weight' and @manual='selected']").Count != 0
-			//    ? WeightEnum.Weighted
-			//    : WeightEnum.Unweighted;
+		        option.WheightOption = doc.SelectNodes("//feature[@name='Weight' and @manual='selected']").Count != 0
+		            ? WeightEnum.Weighted
+		            : WeightEnum.Unweighted;
+		    }
 
-			option.DirectionOption = radioButtonDirected.Checked
-				? DirectionEnum.Directed
-				: radioButtonUndirected.Checked
-					? DirectionEnum.Undirected
-					: throw new Exception("No Direction Selected");
+		    else
+		    {
+                //option.DirectionOption = radioButtonDirected.Checked
+                //    ? DirectionEnum.Directed
+                //    : radioButtonUndirected.Checked
+                //        ? DirectionEnum.Undirected
+                //        : throw new Exception("No Direction Selected");
 
-			option.WheightOption = radioButtonWeighted.Checked
-				? WeightEnum.Weighted
-				: radioButtonUnweighted.Checked
-					? WeightEnum.Unweighted
-					: throw new Exception("No Wheight Selected");
+                //option.WheightOption = radioButtonWeighted.Checked
+                //    ? WeightEnum.Weighted
+                //    : radioButtonUnweighted.Checked
+                //        ? WeightEnum.Unweighted
+                //        : throw new Exception("No Wheight Selected");
 
-			option.SearchOption = !checkBoxSearch.Checked
-				? SearchEnum.NoSearch
-				 : radioButtonBfs.Checked
-					? SearchEnum.Bfs
-					: radioButtonDfs.Checked
-						? SearchEnum.Dfs
-						: throw new Exception("No Search Selected");
+                //option.SearchOption = !checkBoxSearch.Checked
+                //    ? SearchEnum.NoSearch
+                //     : radioButtonBfs.Checked
+                //        ? SearchEnum.Bfs
+                //        : radioButtonDfs.Checked
+                //            ? SearchEnum.Dfs
+                //            : throw new Exception("No Search Selected");
+            }
 
-			var gForm = new GraphForm(option);
+		    var gForm = new GraphForm(option);
 			gForm.Show();
 		}
 
@@ -57,5 +69,32 @@ namespace GraphPresentation
 		{
 			groupBoxSearch.Enabled = checkBoxSearch.Checked;
 		}
-	}
+
+        private void ChkXml_CheckedChanged(object sender, EventArgs e)
+        {
+            ChkManual.Checked = false;
+            Gb_Manual.Enabled = false;
+            Gb_Xml.Enabled = true;
+        }
+
+        private void ChkManual_CheckedChanged(object sender, EventArgs e)
+        {
+            ChkXml.Checked = false;
+            Gb_Manual.Enabled = true;
+            Gb_Xml.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Text files | *.xml";
+            dialog.Multiselect = false;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string path = dialog.FileName;
+                Txt_PathFile.Text = path;
+                _routeFile = path;
+            }
+        }
+    }
 }
